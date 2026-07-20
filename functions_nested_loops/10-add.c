43470d22a921954b474821fa7fb80fa95eaebc5c
root <root@ip-10-42-8-12.ec2.internal>
2026-07-20 03:55:45 +0000

    Task 10: add function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: Sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
