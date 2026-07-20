64652aa950ccfbcfbe25bec7c9783205e968b997
root <root@ip-10-42-8-12.ec2.internal>
2026-07-20 03:57:00 +0000

    Task 11: print_to_98 function

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all integers from n to 98, followed by a new line.
+ * @n: The starting integer.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
+}
